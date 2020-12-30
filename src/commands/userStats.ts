53e63aafe8be04e71fc55a333e3e682c5a135088
WhiteKr <hanjisu2215g@gmail.com>
2020-12-30 11:40:40 +0900

    Coded base of userState.ts

---
+++ src/commands/userStats.ts
@@ -0,0 +1,9 @@
+const PREFIX = require('../../option.json').PREFIX;
+
+const name = 'userstats';
+const usage = `${PREFIX}${name} [@mention]`;
+
+exports.run = (client: any, message: any, args: any) => {
+	
+}
+exports.name = name;
