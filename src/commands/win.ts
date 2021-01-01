26f380ccde6ccfe86b3c6f882a44c7007a720b2b
WhiteKr <hanjisu2215g@gmail.com>
2021-01-02 04:50:35 +0900

    Command 'win' now can set repeat times of winning.

--- src/commands/win.ts
+++ src/commands/win.ts
@@ -38,6 +38,21 @@ exports.run = (client: any, message: any, args: any) => {
 
 	const winnerTier = file.get(`data.${winner}.tier`);
 	const loserTier = file.get(`data.${loser}.tier`);
+
+	let repeatTime = 1;
+	if (args[5] != undefined && args[5].match(/times?/i)) {
+		if (args[4].match(/[^0-9]+/g)) {
+			message.channel.send(`사용법: \`${usage}\``);
+		} else {
+			repeatTime = args[4];
+		}
+	}
+	console.log(`repeatTime = ${repeatTime}`);
+	winlose(winner, loser, message, repeatTime);
+}
+exports.name = name;
+
+function setPointByTierGap(winnerTier: any, loserTier: any) {
 	const winnerTierIndex = TIER.indexOf(winnerTier);
 	const loserTierIndex = TIER.indexOf(loserTier);
 
@@ -47,38 +62,71 @@ exports.run = (client: any, message: any, args: any) => {
 	if (point < 1) point = 1;
 	if (point > 50) point = 50;
 
-	winlose(winner, loser, point, message);
+	return point;
 }
-exports.name = name;
 
-const win = (winner: any, point: number, message: any) => {
+function win(winner: any, point: any) {
 	const file = editJsonFile(`${__dirname}/../../data.json`, { stringify_width: 4 });
+
 	file.set(`data.${winner}.score`, file.get(`data.${winner}.score`) + point);
 	file.set(`data.${winner}.win`, file.get(`data.${winner}.win`) + 1);
-	message.channel.send(`${winner.username}님이 점수를 얻었습니다! ${file.get(`data.${winner}.score`)}점(+${point})`);
 	if (file.get(`data.${winner}.tier`) != TIER[setTier(file.get(`data.${winner}.score`))]) {
-		message.channel.send(`${winner.username}님이 승급하였습니다!\n${file.get(`data.${winner}.tier`)} => ${TIER[setTier(file.get(`data.${winner}.score`))]}`);
 		file.set(`data.${winner}.tier`, TIER[setTier(file.get(`data.${winner}.score`))]);
 	}
 	file.set(`data.${winner}.Odds`, (file.get(`data.${winner}.win`) / (file.get(`data.${winner}.win`) + file.get(`data.${winner}.lose`))) * 100);
 	file.save();
+	console.log(`winner - ${point}`);
+	return point;
 }
-const lose = (loser: any, point: number, message: any) => {
+
+function lose(loser: any, point: any) {
 	const file = editJsonFile(`${__dirname}/../../data.json`, { stringify_width: 4 });
-	if ((file.get(`data.${loser}.score`) - point) <= 0)
+
+	file.set(`data.${loser}.score`, file.get(`data.${loser}.score`) - point);
+	if (file.get(`data.${loser}.score`) <= 0)
 		file.set(`data.${loser}.score`, 0);
-	else
-		file.set(`data.${loser}.score`, file.get(`data.${loser}.score`) - point);
 	file.set(`data.${loser}.lose`, file.get(`data.${loser}.lose`) + 1);
-	message.channel.send(`${loser.username}님이 점수를 잃었습니다.. ${file.get(`data.${loser}.score`)}점(-${point})`);
 	if (file.get(`data.${loser}.tier`) != TIER[setTier(file.get(`data.${loser}.score`))]) {
-		message.channel.send(`저런! ${loser.username}님의 등급이 내려갔습니다.\n${file.get(`data.${loser}.tier`)} => ${TIER[setTier(file.get(`data.${loser}.score`))]}`);
 		file.set(`data.${loser}.tier`, TIER[setTier(file.get(`data.${loser}.score`))]);
 	}
 	file.set(`data.${loser}.Odds`, (file.get(`data.${loser}.win`) / (file.get(`data.${loser}.win`) + file.get(`data.${loser}.lose`))) * 100);
 	file.save();
+
+	console.log(`loser - ${point}`);
+	return point;
 }
-const winlose = (winner: any, loser: any, point: number, message: any) => {
-	win(winner, point, message);
-	lose(loser, point, message);
+
+function winlose(winner: any, loser: any, message: any, repeatTime: number) {
+	const file = editJsonFile(`${__dirname}/../../data.json`, { stringify_width: 4 });
+	const winnerBeforeTier = file.get(`data.${winner}.tier`);
+	const loserBeforeTier = file.get(`data.${loser}.tier`);
+
+	let winnerTotalPoint = 0;
+	let loserTotalPoint = 0;
+
+	if (repeatTime == undefined) {
+		repeatTime = 1;
+	}
+	for (let i = 0; i < repeatTime; i++) {
+		const file = editJsonFile(`${__dirname}/../../data.json`, { stringify_width: 4 });
+		const point = setPointByTierGap(file.get(`data.${winner}.tier`), file.get(`data.${loser}.tier`));
+		winnerTotalPoint += win(winner, point);
+		loserTotalPoint += lose(loser, point);
+
+		if (i == repeatTime - 1) {
+			if (repeatTime != 1)
+				message.channel.send(`Result of <@${winner}> won to <@${loser}> ${repeatTime} times:`);
+			else
+				message.channel.send(`Result of <@${winner}> won to <@${loser}>:`);
+
+			const file = editJsonFile(`${__dirname}/../../data.json`, { stringify_width: 4 });
+			message.channel.send(`${winner.username} got point! ${file.get(`data.${winner}.score`)} points(+${winnerTotalPoint})`);
+			if (winnerBeforeTier != TIER[setTier(file.get(`data.${winner}.score`))])
+				message.channel.send(`${winner.username}님이 승급하였습니다!\n${winnerBeforeTier} => ${TIER[setTier(file.get(`data.${winner}.score`))]}`);
+
+			message.channel.send(`${loser.username} lost points.. ${file.get(`data.${loser}.score`)}점(-${loserTotalPoint})`);
+			if (loserBeforeTier != TIER[setTier(file.get(`data.${loser}.score`))])
+				message.channel.send(`${loser.username}님의 등급이 내려갔습니다..\n${loserBeforeTier} => ${TIER[setTier(file.get(`data.${loser}.score`))]}`);
+		}
+	}
 }
